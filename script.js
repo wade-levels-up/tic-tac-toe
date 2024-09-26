0742d41ac09823285d13c4189de7acbfa31324ae
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-09-26 19:19:38 +1000

    Permit x or o to be entered only in empty grid pos
    The gameboard IIFE now prompts for a marker 'x' or
    'o', and only permits it to be added to an empty
    position on the board. Additionally the board is
    now contructed as a 2D array through a for loop
    as opposed to being a fixed array. This will allow
    me to later populate the board with 'cell' objects
    that store the marker values.

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const gameboard = (function () {
 
-    let board = [[0, 0, 0],
-                 [0, 0, 0],
-                 [0, 0, 0]];
+    // Create our board as a 2D array
+    let rows = 3;
+    let columns = 3;
+    let board = [];
 
-    const placeMarker = function(currentMarker = 'x') {
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(0); // place cell in here later
+        }
+    }
+
+    const placeMarker = function(currentMarker) {
+
+        let marker = prompt('Enter x or o: ');
         let x = prompt('Enter row 1 2 or 3: ');
         let y = prompt('Enter column 1 2 or 3: ');
-        board[x-1][y-1] = currentMarker;
+
+        // Handles if position on board is available or not
+        if (board[x-1][y-1] === 0) {
+            board[x-1][y-1] = marker;
+        } else {
+            alert('Position already taken. Please choose again');
+            placeMarker(currentMarker);
+        }
     }
     const getBoard = () => board;
     const printBoard = () => {console.table(board)};
